867b1090691496604820a9d7830e68cf84672204
Gil80 <just.gil@gmail.com>
2021-10-29 17:58:41 +0300

    program f

---
+++ Lesson-1/assignment-1/Program-F-challenge.py
@@ -0,0 +1,8 @@
+# Fix the following code, without chaning a or b
+
+a = 8
+b = "123"
+
+# print(a+b)  <-- This is the original print instruction
+
+print("The sum of a + b is: " + str(a + int(b)))  # This is the suggested edit to fix the issue.
