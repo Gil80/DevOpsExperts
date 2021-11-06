cc321fa466122af5860b1aa038e9839258018815
Gil80 <just.gil@gmail.com>
2021-11-06 14:33:04 +0200

    program g

---
+++ Lesson-2/assignments/program-g.py
@@ -0,0 +1,13 @@
+# write a program with the following:
+# 1. method named printHello() that print the world "hello"
+# 2. method named calculate() which adds 5+3.2 and prints the result.
+
+def printHello():
+    print("hello")
+
+printHello()
+
+def calculate():
+    print(5+3.2)
+    
+calculate()
