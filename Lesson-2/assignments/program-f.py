45365ccc1ea4212647df461ee453e4174062ae70
Gil80 <just.gil@gmail.com>
2021-11-06 14:27:54 +0200

    program f

---
+++ Lesson-2/assignments/program-f.py
@@ -0,0 +1,6 @@
+# create a program which uses input with the following:
+# 1 - ask a user for his phone number
+# 2 - print the words "phone number" and the phone number the user entered
+
+phone_num = input("Please enter your phone number: \n")
+print("Phone number: " + str(phone_num))
