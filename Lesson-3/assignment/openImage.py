a3710c7b86c352f577c5676f772e23f1c788ab21
Gil80 <just.gil@gmail.com>
2021-11-20 12:13:05 +0200

    added more image attributes

--- Lesson-3/assignment/openImage.py
+++ Lesson-3/assignment/openImage.py
@@ -1,7 +1,10 @@
 from PIL import Image
 
-width = 400
-height = 300
+width = 600
+height = 400
+r = 209
+g = 123
+b = 193
 
-img  = Image.new( mode = "RGB", size = (width, height), color = (209, 123, 193))
+img  = Image.new( mode = "RGB", size = (width, height), color = (r, g, b))
 img.show()
