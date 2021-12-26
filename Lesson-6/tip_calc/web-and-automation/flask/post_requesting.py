492382ebe2a51af68685ad2f9c5acff38b4b8b9a
Gil <just.gil@gmail.com>
2021-12-26 22:21:27 +0200

    removing old folder

--- Lesson-6/tip_calc/web-and-automation/flask/post_requesting.py
+++
@@ -1,4 +0,0 @@
-import requests
-repos = requests.post("http://localhost:5001/add",json={"id": '2',"first":"17","second":"2"})
-
-print(repos.json())
--- Lesson-6/web-and-automation-solutions/flask/get_requesting.py
+++
@@ -1,7 +0,0 @@
-import requests
-try:
-    repos = requests.get("http://127.0.0.1:5001/add/3")
-    print(repos.text)
-    print(repos.status_code)
-except BaseException as ex:
-    print(ex)
